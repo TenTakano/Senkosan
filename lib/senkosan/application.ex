66adb8560b660ade0d2544e8db74e2657f5b3739
TenTakano <takano20771@gmail.com>
2020-08-23 21:35:40 +0900

    refactor: refactor session observer with Agent module (#8)

--- lib/senkosan/application.ex
+++ lib/senkosan/application.ex
@@ -2,10 +2,15 @@ defmodule Senkosan.Application do
   use Application
 
   def start(_type, _args) do
-    children = [
-      {Senkosan.Consumer, []},
-      {Senkosan.SessionObserver, []}
-    ]
+    children =
+      if Mix.env() != :test do
+        [
+          {Senkosan.Consumer, []},
+          {Senkosan.SessionObserver, []}
+        ]
+      else
+        []
+      end
 
     opts = [strategy: :one_for_one, name: Senkosan.Supervisor]
     Supervisor.start_link(children, opts)
--- lib/senkosan/consumer.ex
+++ lib/senkosan/consumer.ex
@@ -12,10 +12,10 @@ defmodule Senkosan.Consumer do
   end
 
   def handle_event({:VOICE_STATE_UPDATE, msg, _}) do
-    IO.inspect msg
-    if SessionObserver.update(msg) == :join do
+    %{channel_id: channel_id, member: %{user: user}} = msg
+    if SessionObserver.update({channel_id, user.id}) == :join do
       Utils.apply_bot_usage(
-        Map.get(msg.member.user, :bot, false),
+        Map.get(user, :bot, false),
         fn -> Api.create_message(@default_text_channel, "おかえりなのじゃ！") end
       )
     end
