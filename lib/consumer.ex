d86906a238bd4f952097be2e720f34a0b7577f36
TenTakano <takano20771@gmail.com>
2020-09-03 07:17:16 +0900

    refactor: Revise modules structure (#10)
    * move application root
    
    * move root module
    
    * move voice state handling code
    
    * move util module
    
    * move observer module

---
+++ lib/consumer.ex
@@ -0,0 +1,15 @@
+defmodule Senkosan.Consumer do
+  use Nostrum.Consumer
+
+  def start_link do
+    Consumer.start_link(__MODULE__)
+  end
+
+  def handle_event({:VOICE_STATE_UPDATE, msg, _}) do
+    Senkosan.VoiceState.parse(msg)
+  end
+
+  def handle_event(_event) do
+    :noop
+  end
+end
