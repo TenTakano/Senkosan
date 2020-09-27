df9f8cb0660a0939c05b1ae66e16520cb4d40965
TenTakano <takano20771@gmail.com>
2020-09-27 17:55:51 +0900

    impl temporaly

--- lib/ets/voice_state.ex
+++ lib/ets/voice_state.ex
@@ -1,32 +1,47 @@
 defmodule Senkosan.Ets.VoiceState do
   use Agent
 
+  alias Nostrum.Struct.Message
+
   @type t :: __MODULE__.t
 
   @enforce_keys [:name, :is_bot]
   defstruct [:name, :is_bot, :channel_id, is_greeted: false]
 
-  def start_link() do
-    table = :ets.new(:voice_state, [:ordered_set, :protected])
-    Agent.start_link(fn -> table end, name: __MODULE__)
-    table
+  @table_name :senkosan_voice_state
+
+  @doc """
+  Creates a table to contain the voice states and insertes user states.
+  Each user state is fetched by Discord guild member list API
+  """
+  @spec init(integer) :: :ok
+  def init(guild_id) do
+    :ets.new(@table_name, [:ordered_set, :protected, :named_table])
+
+    guild_id
+    |> Nostrum.Api.list_guild_members!(limit: 1000)
+    |> Enum.each(fn %{user: user} ->
+      attrs = %__MODULE__{
+        name: user.username,
+        is_bot: user.bot
+      }
+      :ets.insert(@table_name, {user.id, attrs})
+    end)
+
+    :ok
   end
 
-  @spec insert(integer, t) :: boolean
-  def insert(user_id, attrs) do
-    Agent.get(__MODULE__, &(&1))
-    |> :ets.insert_new({user_id, attrs})
-  end
+  @spec get_op(map) :: atom
+  def get_op(%{channel_id: new_channel_id, user_id: user_id} = _) do
+    user = :ets.lookup_element(@table_name, user_id, 2)
 
-  @spec fetch(integer) :: t
-  def fetch(user_id) do
-    Agent.get(__MODULE__, &(&1))
-    |> :ets.lookup_element(user_id, 2)
-  end
-
-  @spec update(integer, t) :: boolean
-  def update(user_id, attrs) do
-    Agent.get(__MODULE__, &(&1))
-    |> :ets.update_element(user_id, {2, attrs})
+    case {user, new_channel_id} do
+      {%{channel_id: prev}, new} when prev == new ->
+        :mic_op
+      {%{channel_id: nil}, _} ->
+        :join
+      {_, nil} ->
+        :left
+    end
   end
 end
